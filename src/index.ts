ac8e940e5252e65b083613dc1f8ae3bc04bb5c64
David Chavez <david@dcvz.io>
2019-03-07 22:41:56 +0100

    Setup package

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+// @ts-ignore
+import { observable } from 'mobx'
+// @ts-ignore
+import { AsyncStorage } from 'react-native'
+
+const STORAGE_KEY = '@DNCStorage'
+
+export default class Storage {
+  static cache = observable.map({})
+
+  static async read() {
+    const storage = await AsyncStorage.getItem(STORAGE_KEY)
+    if (storage) {
+      this.cache.replace(JSON.parse(storage))
+    }
+    return this.cache.toJS()
+  }
+
+  static write() {
+    return AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(this.cache.toJS()))
+  }
+
+  static clear() {
+    this.cache.clear()
+    return AsyncStorage.removeItem(STORAGE_KEY)
+  }
+
+  static get(key: string) {
+    if (!key) {
+      throw new Error('Missing key')
+    }
+    return this.cache.get(key)
+  }
+
+  static set(key: string, value: any) {
+    if (!key) {
+      throw new Error('Missing key')
+    }
+    this.cache.set(key, value)
+    return this.write()
+  }
+
+  static setFlag(key: string) {
+    return this.set(key, 1)
+  }
+
+  static getFlag(key: string) {
+    return !!this.get(key)
+  }
+
+  static delete(key: string) {
+    this.cache.delete(key)
+  }
+}
